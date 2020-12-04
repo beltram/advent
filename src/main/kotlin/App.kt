b6e52e70429d43cd18981dfd03971fc6a901e71a
beltram.maldant <beltram.maldant@soprabanking.com>
2020-12-04 14:06:07 +0100

    day 4 part 2 ok

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -4,5 +4,6 @@ fun main() {
 //    println("Day 2 (part 2): ${day2Part2()}")
 //    println("Day 3 (part 1): ${day3Part1()}")
 //    println("Day 3 (part 2): ${day3Part2()}")
-    println("Day 4 (part 1): ${day4Part1()}")
+//    println("Day 4 (part 1): ${day4Part1()}")
+    println("Day 4 (part 2): ${day4Part2()}")
 }
--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,13 +1,11 @@
 import Field.Companion.toField
 import Passport.Companion.toPassport
 
-fun day4Part1(): Int {
-    return input("day4")
-        .split("\n\n")
-        .map { it.toPassport() }
-        .filter { it.isValidPart1() }
-        .count()
-}
+fun day4Part1() = allPassports().filter { it.isValidPart1() }.count()
+
+fun day4Part2() = allPassports().filter { it.isValidPart2() }.count()
+
+private fun allPassports() = input("day4").split("\n\n").map { it.toPassport() }
 
 data class Passport(val fields: List<Field>) {
     companion object {
@@ -21,10 +19,13 @@ data class Passport(val fields: List<Field>) {
         }
     }
 
-    fun isValidPart1(): Boolean {
-        return Key.values()
-            .filter { it.isRequired }
-            .all { k -> k in fields.map { it.key } }
+    private val keys = fields.map { it.key }
+    private val requiredKeys = Key.values().filter { it.isRequired }
+
+    fun isValidPart1() = requiredKeys.all { k -> k in keys }
+
+    fun isValidPart2(): Boolean {
+        return isValidPart1() && fields.all { it.isValid() }
     }
 }
 
@@ -35,15 +36,55 @@ data class Field(val key: Key, val value: String) {
             return Field(Key.valueOf(key.toUpperCase()), value)
         }
     }
+
+    fun isValid() = key.validator(value)
 }
 
-enum class Key(val isRequired: Boolean) {
-    BYR(true),
-    IYR(true),
-    EYR(true),
-    HGT(true),
-    HCL(true),
-    ECL(true),
-    PID(true),
+enum class Key(val isRequired: Boolean, val validator: (String) -> Boolean = { true }) {
+    BYR(true, { s -> s.toIntOrNull()?.let { it in 1920..2002 } ?: false }),
+    IYR(true, { s -> s.toIntOrNull()?.let { it in 2010..2020 } ?: false }),
+    EYR(true, { s -> s.toIntOrNull()?.let { it in 2020..2030 } ?: false }),
+    HGT(true, { Height(it).isValid() }),
+    HCL(true, { hairColorRegex.matches(it) }),
+    ECL(true, { it in eyeColors }),
+    PID(true, { passportIdRegex.matches(it) }),
     CID(false);
+
+    companion object {
+        val hairColorRegex = Regex("#[0-9a-f]{6}")
+        val eyeColors = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+        val passportIdRegex = Regex("[0-9]{9}")
+    }
+
+    private class Height(val input: String) {
+        val unit: HeightUnit? by lazy {
+            when {
+                input.endsWith(HeightUnit.CM.label) -> HeightUnit.CM
+                input.endsWith(HeightUnit.IN.label) -> HeightUnit.IN
+                else -> null
+            }
+        }
+
+        val value: Int? by lazy {
+            when (unit) {
+                HeightUnit.CM -> input.substringBefore(HeightUnit.CM.label)
+                HeightUnit.IN -> input.substringBefore(HeightUnit.IN.label)
+                else -> null
+            }?.toIntOrNull()
+        }
+
+        fun isValid(): Boolean {
+            return when (unit) {
+                HeightUnit.CM -> value?.let { it in 150..193 } ?: false
+                HeightUnit.IN -> value?.let { it in 59..76 } ?: false
+                else -> false
+            }
+        }
+
+        enum class HeightUnit {
+            CM, IN;
+
+            val label get() = name.toLowerCase()
+        }
+    }
 }
